f86429173f63dfbd2d3ea5c6290ce417d951fcfe
Dan Bennett <naebeth@googlemail.com>
2013-03-06 12:29:35 +0100

    improved example page

--- index.php
+++ index.php
@@ -45,6 +45,7 @@ if(!defined("WP_PLUGIN_URL")){
 		}
 		#container{
 			max-width: 900px;
+			padding: 1em 0.5em 0 0.5em;
 			margin: 0 auto;
 		}
 		.skype_icon img{
@@ -75,13 +76,30 @@ if(!defined("WP_PLUGIN_URL")){
 			top: -0.2em;
 		}
 		</style>
-		<title>Status-Aware Skype Link</title>
+		<title>Status-Aware Skype Links</title>
 	</head>
 		<body>
 			<div id="container">
-				<?php
-				echo $WPSS->skype($args);
-				?>
+				<h2>Status-Aware Skype Links!</h2>
+				<div id="example">
+					<?php echo $WPSS->skype($args); ?>
+				</div>
+				<div id="instructions">
+					<h3>How to Use This Example</h3>
+					<ul>Form a query string with any combination of:
+						<li>name - the name shown in the link (e.g. Sales or Dan)</li>
+						<li>username - the username of the Skype account you want to link to</li>
+						<li>type - whether the link should initiate a call or a chat</li>
+						<li>size - the image size to use (default images are 16, 32 or 64px squares)</li>
+						<li>backups - a comma-separated list of alternatives to 'username' that are used if 'username' is offline</li>
+						Example: <a href="?username=echo123&name=Echo%20Test&size=32">?username=echo123&name=Echo%20Test&size=32</a>
+					</ul>
+					<p>
+						For your Skype status to be visible to the API you'll need to check the following option:
+						<br /><br />
+						<img src="images/status-settings.png" width="723" height="521" />
+					</p>
+				</div>
 			</div>
 		</body>
 	</html>
