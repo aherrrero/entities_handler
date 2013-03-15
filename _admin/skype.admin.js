3bf6b03b8eb4c43a4936fe83217058846286317f
Dan Bennett <naebeth@googlemail.com>
2013-03-15 14:14:26 +0100

    more work on saving settings in WP admin
    Link Parameters page now fully working, PHP and JS [hopefully] won't
    need changing for the other two pages

--- _admin/skype.admin.js
+++ _admin/skype.admin.js
@@ -1,3 +1,4 @@
+var timers = {};
 jQuery(document).ready(function(){
 	jQuery("#skype_settings #skype_save").click(function(e){
 		e.preventDefault();
@@ -32,11 +33,49 @@ function do_ajax(values){
 		url:"/wp-content/plugins/WPSkypeStatus/_admin/skype.ajax.php",
 		data:{skype_set:null, data:values},
 		error:function(req, status, err){
-			console.log(req, status, err);
+			// console.log(status+": ", req);
+			show_message("An error ("+err+") occurred when trying to save your changes.", "error", false);
 		},
 		success:function(data, status, req){
-			console.log(data, status, req);
-			//<div id='message' class='updated fade'><p></p></div>
+			data = JSON.parse(data);
+			var _message = "Settings saved with %d rows updated.", _type = "updated";
+			for(var i = 0; i < data.length; i++){
+				if(data[i] === false || data[i] === 'false'){
+					// console.log(status+": ", req);
+					_message = "An error occurred while trying to update the database.";
+					_type = "error";
+				} else {
+					_message = _message.replace('%d', data[i]);
+				}
+				show_message(_message, _type);
+			}
+			_message = _type = null;
 		}
 	});
+}
+
+function show_message(message, type, close){
+	close = (typeof close === "undefined");
+	type = (type.toLowerCase() === 'updated' || type.toLowerCase() === 'error') ? type : "updated";
+	_id = "message_"+Math.floor((Math.random()*100000)+1);
+	while(jQuery(_id).length > 0){
+		_id = "message_"+Math.floor((Math.random()*100000)+1);
+	}
+	jQuery("#ajax-response").append("<div id='"+_id+"' class='"+type+" fade'><p>"+message+"</p></div>");
+	if(close){
+		timers[_id] = setTimeout(function(){
+			close_message(_id);
+		}, 5000);
+	}
+}
+
+function close_message(id){
+	jQuery("#"+id).hide(
+		400,
+		function(){
+			jQuery("#"+id).remove();
+			clearTimeout(timers[id]);
+			delete timers[id];
+		}
+	);
 }
